910a75c06ea59b03675edf98a285998158e4a764
Teerut26 <teerutsr@gmail.com>
2022-05-16 00:32:04 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Search from "./components/Search";
+import Player from "./components/Player";
+import domtoimage from "dom-to-image";
+import { copyImageToClipboard } from "copy-image-clipboard";
+import { saveAs } from "file-saver";
+import { Result } from "./interfaces/response";
+
+function App() {
+    let area = useRef<HTMLDivElement>(null);
+    const [DataSelected, setDataSelected] = useState<Result | undefined>();
+    const [Loading, setLoading] = useState<boolean>(false);
+
+    const handleDownload = async () => {
+        setLoading(true);
+        const dataUrl = await domtoimage.toPng(area.current as any);
+        setLoading(false);
+        saveAs(dataUrl, `ophtusify-${DataSelected?.trackId}.png`);
+    };
+
+    return (
+        <div>
+            <Search onSelect={(value) => setDataSelected(value)} />
+            <div className="flex flex-col items-center p-2 gap-3 scale-[0.6] sm:scale-75 md:scale-90 lg:scale-100">
+                {DataSelected ? (
+                    <>
+                        <Player
+                            Data={DataSelected}
+                            onRef={(value) => (area = value)}
+                        />
+                        <div
+                            className="bg-[#007AFF] hover:bg-[#007AFF]/80 cursor-pointer select-none text-white px-3 py-2 rounded-[0.8rem] font-semibold w-full max-w-[585px] mx-auto flex justify-center"
+                            onClick={() => handleDownload()}
+                        >
+                            {Loading ? "Loading..." : "Download"}
+                        </div>
+                        <div
+                            className="bg-[#FF0000] hover:bg-[#FF0000]/80 cursor-pointer select-none text-white px-3 py-2 rounded-[0.8rem] font-semibold w-full max-w-[585px] mx-auto flex justify-center"
+                            onClick={() => {
+                                setDataSelected(undefined);
+                            }}
+                        >
+                            Clear
+                        </div>
+                    </>
+                ) : (
+                    ""
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Search.tsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+import { Response, Result } from "../interfaces/response";
+
+interface Props {
+    onSelect: (music: Result) => void;
+}
+
+const Search: React.FC<Props> = ({ onSelect }) => {
+    const [keyWord, sekeyWord] = useState<string>("");
+    const [musicList, setmMusicList] = useState<Result[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const getData = async () => {
+        setLoading(true);
+        let { data } = await axios.get<Response>(
+            `https://itunes.apple.com/search?term=${keyWord}&limit=20&kind=song`
+        );
+        setmMusicList(data.results.filter((item) => item.kind === "song"));
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getData();
+    }, [keyWord]);
+
+    return (
+        <div className="bg-blue-400 p-3 flex flex-col w-full">
+            <div className="w-full relative">
+                <input
+                    onChange={(e) => sekeyWord(e.target.value)}
+                    type="search"
+                    className={`focus:outline-none p-2 shadow-md ${
+                        keyWord.length > 0 ? "rounded-t-xl" : "rounded-xl"
+                    } w-full`}
+                    placeholder="ค้นหาเพลง"
+                />
+                {keyWord.length > 0 ? (
+                    <div className="absolute bg-white rounded-b-xl right-0 left-0 p-3 flex flex-col gap-2 divide-y divide-light-500 shadow-md z-20">
+                        {!loading
+                            ? musicList.map((item, index) => (
+                                  <div
+                                      onClick={() => {
+                                          onSelect(item);
+                                          sekeyWord("");
+                                      }}
+                                      key={index}
+                                      className="flex items-center gap-2 hover:bg-gray-200 cursor-pointer select-none"
+                                  >
+                                      <div className="">
+                                          <img
+                                              className="w-[60px]"
+                                              src={item.artworkUrl60}
+                                              alt=""
+                                          />
+                                      </div>
+                                      <div className="flex flex-col flex-1 truncate">
+                                          <div className="truncate">
+                                              {item.trackName}
+                                          </div>
+                                          <div>{item.artistName}</div>
+                                      </div>
+                                  </div>
+                              ))
+                            : "Loading..."}
+                    </div>
+                ) : (
+                    ""
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Search;
