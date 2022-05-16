f5016da4ad7acde77ad3fc31d42f44d689d469ea
Teerut26 <teerutsr@gmail.com>
2022-05-16 11:20:48 +0700

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {}
+
+const Footer: React.FC<Props> = ({ }) => {
+    return (
+        <div className="text-center">
+            แนะนำให้ใช้งานบน Application Google Chrome หรือ Google Chrome
+            Desktop
+        </div>
+    );
+};
+
+export default Footer;
